93690c38d1dd3f61afd87f5c8ccf615e05c03d37
mangxiao <mangxiao2018@126.com>
2020-09-29 12:25:47 +0800

    针对订单的增删改查

--- src/main/java/com/mangxiao/seata/sample/mapper/OrdersMapper.java
+++ src/main/java/com/mangxiao/seata/sample/mapper/OrdersMapper.java
@@ -7,21 +7,21 @@ import java.util.List;
 
 @Mapper
 public interface OrdersMapper {
-//    @Results({
-//            @Result(property = "id", column = "id", id = true),
-//            @Result(property = "orderId", column = "order_id"),
-//            @Result(property = "createTime", column = "create_time"),
-//            @Result(property = "updateTime", column = "update_time"),
-//            @Result(property = "createUserId", column = "create_user_id"),
-//            @Result(property = "updateUserId", column = "update_user_id"),
-//            @Result(property = "yn", column = "yn")
-//    })
+    @Results(id="OrdersMap", value = {
+            @Result(property = "id", column = "id", id = true),
+            @Result(property = "orderId", column = "order_id"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "updateTime", column = "update_time"),
+            @Result(property = "createUserId", column = "create_user_id"),
+            @Result(property = "updateUserId", column = "update_user_id"),
+            @Result(property = "yn", column = "yn")
+    })
     /**
      * 根据订单号获取该条订单信息
      * @param orders
      * @return
      */
-    @Select("SELECT id,order_id,create_time,update_time,create_user_id,update_user_id,yn FROM orders WHERE id = #{orders.id}")
+    @Select("SELECT id,order_id,create_time,update_time,create_user_id,update_user_id,yn FROM orders WHERE order_id = #{orderId}")
     Orders getOrders(Orders orders);
 
     /**
@@ -29,6 +29,7 @@ public interface OrdersMapper {
      * @return
      */
     @Select("SELECT id,order_id,create_time,update_time,create_user_id,update_user_id,yn FROM orders")
+    @ResultMap("OrdersMap")
     List<Orders> getAll();
 
     /**
@@ -45,7 +46,7 @@ public interface OrdersMapper {
      * @return
      */
     @Delete("DELETE FROM orders WHERE order_id=#{orderId}")
-    int deleteOrder(Long orderId);
+    int deleteOrder(@Param("orderId") Long orderId);
 
     /**
      * 根据订单号更新该条订单信息
