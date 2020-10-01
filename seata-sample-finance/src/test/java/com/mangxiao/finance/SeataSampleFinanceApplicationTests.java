caa97b35970f5ecb63e1dba971ed4e1e9e16a5ac
mangxiao <mangxiao2018@126.com>
2020-10-02 07:52:09 +0800

    finance模块

---
+++ seata-sample-finance/src/test/java/com/mangxiao/finance/SeataSampleFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangxiao.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeataSampleFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
