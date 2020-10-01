6d20b04b4df44d576abd7a74dfc278da4f382728
mangxiao <mangxiao2018@126.com>
2020-10-01 21:32:32 +0800

    seata库存系统

---
+++ seata-sample-storage/src/main/java/com/mangxiao/storage/SeataSampleStorageApplication.java
@@ -0,0 +1,13 @@
+package com.mangxiao.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataSampleStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeataSampleStorageApplication.class, args);
+	}
+
+}
