5deeb0db5f0ca33a708886cdedda0344636bc650
mangxiao <mangxiao2018@126.com>
2020-10-03 22:12:45 +0800

    sku

---
+++ seata-sample-finance/src/main/java/com/mangxiao/finance/model/Sku.java
@@ -0,0 +1,52 @@
+package com.mangxiao.finance.model;
+
+import java.util.Date;
+
+public class Sku {
+
+    private Long id;
+
+    private String skuName;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Long createUserId;
+
+    private Long updateUserId;
+
+    private int yn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSkuName() {
+        return skuName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public Long getCreateUserId() {
+        return createUserId;
+    }
+
+    public Long getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public int getYn() {
+        return yn;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
